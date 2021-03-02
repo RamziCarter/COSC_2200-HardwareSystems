13b14fc3b574127e152357afe27ce600a9d33d4b
RamziCarter <56376705+RamziCarter@users.noreply.github.com>
2021-03-02 15:13:24 -0600

    Add files via upload

---
+++ lab3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int getBitCounts(int X, int Y)
+	{
+	
+	int number = 1 << Y;
+	x = x & number; 
+	if(number != 0)
+		{
+			return true;
+		}
+
+	else	
+		{
+			return false;
+		}
+
+int main() 
+	{
+	
+	int a;
+	int b;
+	int i;
+
+	for(i = 0; i < 32; i++)
+		{
+			
+
+		}	
+	}
+
